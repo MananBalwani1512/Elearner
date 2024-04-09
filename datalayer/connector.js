4a08d07c2efc1417406b56ec07df3b8135925b6a
MananBalwani1512 <98649645+MananBalwani1512@users.noreply.github.com>
2024-04-09 19:53:09 +0530

    Update connector.js

--- datalayer/connector.js
+++ datalayer/connector.js
@@ -2,7 +2,7 @@ const { Client } = require('pg');
 async function getConnection()
 {
     var connection = new Client({
-        "connectionString" : "postgresql://Elearning_owner:enqpMoDgm50C@ep-mute-sunset-a5i7p0xq.us-east-2.aws.neon.tech/Elearning?sslmode=require"
+        "connectionString" : "postgresql://Elearning_owner:{Your Password}@ep-mute-sunset-a5i7p0xq.us-east-2.aws.neon.tech/Elearning?sslmode=require"
     });
     connection.connect();
     return connection;
